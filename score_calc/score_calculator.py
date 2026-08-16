fa88edbd28ecdf5e53649733920c138ea9663fd2
Sughosh Sai Prasad <sughosh.sp@gmail.com>
2026-08-16 16:55:35 +0530

    created a website to show sccoreboard after ctf ends

--- score_calc/score_calculator.py
+++ score_calc/score_calculator.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 MAX_POINTS_CTF = 430
-MAX_POINTS_ROUND1 = 500
+MAX_POINTS_PHASE1 = 500
 
 CTFD_URL = "enter CTFd url here"
 CTFD_ADMIN_TOKEN = "ctfd token here"
@@ -10,6 +10,8 @@ CTFD_ADMIN_TOKEN = "ctfd token here"
 #sorted in order of score 
 PHASE1_STATE = "here phase 1 state json here"
 LAST_COMPLETED_ROUND = "4" #i guess
+
+SCORES_OUTPUT_PATH = "enter path to scores json here"
 
 headers = {
     "Authorization": CTFD_ADMIN_TOKEN,
@@ -20,7 +22,7 @@ response = requests.get(CTFD_URL, headers=headers)
 scoreboard_data = response.json()
 
 ctf_team_dict = {}
-round1_team_dict = {}
+phase1_team_dict = {}
 
 for team in scoreboard_data['data']:
                                     #normalise score
@@ -30,12 +32,12 @@ with open(PHASE1_STATE, "r") as f:
     phase1_data = json.load(f)
 
     for team_id, score in phase1_data['rounds'][LAST_COMPLETED_ROUND].items():
-        round1_team_dict[team_id] = score/MAX_POINTS_ROUND1
+        phase1_team_dict[team_id] = score/MAX_POINTS_PHASE1
 
 combined_team_scores = {}
 
 
-for team_id, phase1_score in round1_team_dict.items():
+for team_id, phase1_score in phase1_team_dict.items():
     
     ctf_score = ctf_team_dict.get(team_id, 0)
 
@@ -43,6 +45,25 @@ for team_id, phase1_score in round1_team_dict.items():
     combined_team_scores[team_id] = (phase1_score + ctf_score) * 50
                                                                                             
 final_scoreboard = dict(sorted(combined_team_scores.items(), key=lambda item: item[1], reverse=True))
+
+
+export_data = {
+     "ctf": [
+            {"team": team, "raw_score": round(score * MAX_POINTS_CTF), "normalized": round(score, 2)}
+            for team, score in ctf_team_dict.items()
+        ],
+    "round1": [
+        {"team": team, "raw_score": round(score * MAX_POINTS_PHASE1), "normalized": round(score, 2)}
+        for team, score in phase1_team_dict.items()
+    ],
+    "combined": [
+        {"team": team, "score": round(score, 2)}
+        for team, score in final_scoreboard.items()
+    ],
+}
+
+with open(SCORES_OUTPUT_PATH, "w") as f:
+    json.dump(export_data, f, indent=2)
 
 
 
